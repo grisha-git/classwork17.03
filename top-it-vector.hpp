15989ce18ea21c1e1b9476cc05d64e28514bec61
grisha-git <donkeevgrisa@gmail.com>
2026-03-17 11:11:48 +0300

    Test interface

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,9 +1,30 @@
 #ifndef TOP_IT_VECTOR_HPP
 #define TOP_IT_VECTOR_HPP
-template< class T >
-struct Vector
+#include <cstddef>
+namespace topit
 {
-  T* data;
-  size_t size, capacity;
-};
+  template< class T >
+  struct Vector
+  {
+    Vector();
+    ~Vector();
+
+  private:
+    T* data_;
+    size_t size_, capacity_;
+  };
+}
+template< class T >
+topit::Vector< T >::~Vector()
+{
+
+}
+template< class T >
+topit::Vector< T >::Vector():
+  data_(),
+  size_(),
+  capacity_()
+{
+  
+}
 #endif
